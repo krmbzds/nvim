bc1a9860d6d2da0b5832b19579fa3f8f83222585
Kerem Bozdas <krmbzds.github@gmail.com>
2022-03-10 12:58:43 +0300

    Add colorizer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,6 +54,7 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "Mofiqul/dracula.nvim"
+  use "br1anchen/nvim-colorizer.lua"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
