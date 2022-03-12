ad545c4459772d95314da709610104d884730d4f
Kerem Bozdas <krmbzds.github@gmail.com>
2022-03-12 20:45:50 +0300

    Add FixCursorHold

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,6 +54,7 @@ return packer.startup(function(use)
   use "akinsho/toggleterm.nvim"
   use "ahmedkhalf/project.nvim"
   use "lewis6991/impatient.nvim"
+  use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
