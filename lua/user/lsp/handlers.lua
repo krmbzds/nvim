c7d6c7dfbd0b9766ed69d0cc89d13cbbd7359eb5
Kerem Bozdas <krmbzds.github@gmail.com>
2022-03-13 22:04:35 +0300

    Handle case where cmp_nvim_lsp not installed

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -89,10 +89,10 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
+if status_ok then
+  M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+else
   return
 end
-
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 return M
