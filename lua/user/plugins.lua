25ccc06ab9f1567b4e08ef4c08edffdc744e47c9
Kerem Bozdas <krmbzds.github@gmail.com>
2022-03-13 12:03:14 +0300

    Add alpha

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,6 +54,7 @@ return packer.startup(function(use)
   use("akinsho/toggleterm.nvim")
   use("ahmedkhalf/project.nvim")
   use("lewis6991/impatient.nvim")
+  use("goolord/alpha-nvim")
   use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
 
   -- Colorschemes
