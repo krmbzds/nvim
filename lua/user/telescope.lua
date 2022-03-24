1db7cbfb64e3161740e5e205921cbebc6cd2a71b
Kerem Bozdas <krmbzds.github@gmail.com>
2022-03-24 22:23:52 +0300

    Add keymaps to delete buffers from telescope
    Use <leader>b to show open buffers,
    <C-j> and <C-k> to move, <C-d> to delete.

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -26,12 +26,14 @@ telescope.setup({
         ["<Up>"] = actions.move_selection_previous,
 
         ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
+        ["<C-s>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
         ["<C-t>"] = actions.select_tab,
 
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
+        ["<C-d>"] = require("telescope.actions").delete_buffer,
+
+        -- ["<C-u>"] = actions.preview_scrolling_up,
+        -- ["<C-d>"] = actions.preview_scrolling_down,
 
         ["<PageUp>"] = actions.results_scrolling_up,
         ["<PageDown>"] = actions.results_scrolling_down,
