5d47a3a5589b80a46562561550645d11daa32d2e
Kerem Bozdas <krmbzds.github@gmail.com>
2022-03-25 20:09:10 +0300

    Add sort

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -102,6 +102,9 @@ return packer.startup(function(use)
   -- Git
   use("lewis6991/gitsigns.nvim")
 
+  -- Other
+  use("sQVe/sort.nvim")
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
