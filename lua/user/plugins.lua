7a4d3dba7ddfeba174878faa86a1d6b6a4b54a6a
Kerem Bozdas <krmbzds.github@gmail.com>
2022-03-27 04:40:14 +0300

    Add gitlinker

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -105,6 +105,7 @@ return packer.startup(function(use)
   -- Git
   use("lewis6991/gitsigns.nvim")
   use("f-person/git-blame.nvim")
+  use("ruifm/gitlinker.nvim")
 
   -- Other
   use("sQVe/sort.nvim")
