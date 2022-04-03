6291fd02085708a8fe8bac83f35fb0dedcd600c5
Kerem Bozdas <krmbzds.github@gmail.com>
2022-04-03 23:23:19 +0300

    Set treesitter parsers

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,31 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {
+    "bash",
+    "c",
+    "cmake",
+    "comment",
+    "cpp",
+    "css",
+    "dockerfile",
+    "hcl",
+    "html",
+    "http",
+    "javascript",
+    "json",
+    "json5",
+    "jsonc",
+    "lua",
+    "make",
+    "python",
+    "ruby",
+    "scss",
+    "toml",
+    "typescript",
+    "vim",
+    "yaml",
+  },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
