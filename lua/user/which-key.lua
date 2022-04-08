d94e0052607997d34b2c1c419b08bfc4009cdf76
Kerem Bozdas <krmbzds.github@gmail.com>
2022-04-08 06:49:32 +0300

    Add gitsigns prev/next hunk keymaps

--- lua/user/which-key.lua
+++ lua/user/which-key.lua
@@ -18,7 +18,7 @@ local setup = {
       motions = true, -- adds help for motions
       text_objects = true, -- help for text objects triggered after entering an operator
       windows = true, -- default bindings on <c-w>
-      nav = true, -- misc bindings to work with windows
+      nav = false, -- misc bindings to work with windows
       z = true, -- bindings for folds, spelling and others prefixed with z
       g = true, -- bindings for prefixed with g
     },
