6daf3231742d8557b80a5e07f022928d55f4178e
Kerem Bozdas <krmbzds.github@gmail.com>
2022-12-15 02:30:37 +0300

    Add keymap to move buffers

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -34,6 +34,8 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-A-l>", "<cmd>BufferLineMoveNext<CR>", opts)
+keymap("n", "<S-A-h>", "<cmd>BufferLineMovePrev<CR>", opts)
 
 -- Navigate tabs
 keymap("n", "<A-l>", ":tabnext<CR>", opts)
