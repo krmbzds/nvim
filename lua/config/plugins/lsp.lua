772f8e7a19699b8d03ebb01489f4b0465b23976c
Kerem Bozdas <krmbzds.github@gmail.com>
2025-11-10 10:38:08 +0300

    Update lsp.lua

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -152,7 +152,7 @@ function M.config()
       vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
       vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
       vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "<space>f", function()
+      vim.keymap.set("n", "<space>lf", function()
         vim.lsp.buf.format({ async = true })
       end, opts)
     end,
