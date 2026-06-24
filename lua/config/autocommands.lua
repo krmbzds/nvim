14b40cf8e9ec2f27147830487f0d4e886fa55929
Kerem Bozdas <krmbzds.github@gmail.com>
2026-06-24 17:25:30 +0300

    Use vim.hl.on_yank instead of deprecated vim.highlight

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -40,7 +40,7 @@ api.nvim_create_autocmd({ "BufWinEnter" }, {
 
 api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
-    vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+    vim.hl.on_yank({ higroup = "Visual", timeout = 200 })
   end,
 })
 
